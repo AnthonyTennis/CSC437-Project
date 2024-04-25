39018b41414d57fcbd01bc2e8192898a23319a52
anthonytennis <teciorowskianthony3@gmail.com>
2024-04-24 20:49:33 -0700

    Complete through A8

---
+++ public/src/destinationLoader.js
@@ -0,0 +1,12 @@
+// destinationLoader.js
+function loadDestination(url, container) {
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            container.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error loading the destination details:', error);
+            container.innerHTML = "<p>Error loading details. Please try again later.</p>";
+        });
+}
